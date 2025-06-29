78d09f17a7893e12e9a2daa56c535ae068381937
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2025-06-29 18:37:27 +0100

    fix positions

--- positions.py
+++ positions.py
@@ -4,21 +4,21 @@ ENDGAME_POSITIONS = {
     'lucena': {
         'name': 'Lucena Position',
         'description': 'Rook + Pawn vs Rook - winning technique with bridge building',
-        'fen': '4R3/8/8/8/8/3k4/3P4/3K1r2 w - - 0 1',
+        'fen': '3K4/3P1k2/8/8/8/8/4R3/2r5 w - - 0 1',
         'goal': 'Win by promoting the pawn using the bridge technique',
         'user_plays': 'white'
     },
     'philidor': {
         'name': 'Philidor Position', 
         'description': 'Rook + Pawn vs Rook - drawing technique with passive defense',
-        'fen': '4k3/8/8/8/8/8/4P3/4K2r w - - 0 1',
+        'fen': '8/8/8/8/4pk2/R7/7r/4K3 w - - 0 1',
         'goal': 'Draw by maintaining passive rook defense on the back rank',
-        'user_plays': 'black'
+        'user_plays': 'white'
     },
     'king_pawn_vs_king': {
         'name': 'King + Pawn vs King',
         'description': 'Opposition and breakthrough technique',
-        'fen': '8/8/8/4k3/8/4K3/4P3/8 w - - 0 1',
+        'fen': '8/4k3/8/8/3KP3/8/8/8 w - - 0 1',
         'goal': 'Win by using opposition to advance the pawn to promotion',
         'user_plays': 'white'
     }
